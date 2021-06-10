24dc2a27eef74c947e585b382f7a6470b013b235
Nishant Baheti <nishantbaheti.it19@gmail.com>
2021-06-11 01:07:07 +0530

    Cython Demo

---
+++ CythonDemo/prime.pyx
@@ -0,0 +1,22 @@
+def print_prime_number(int last):
+    
+    cdef int digit,is_prime,cursor
+    
+    digit = 2
+    while digit <= last :
+
+        is_prime = 1
+        cursor = 2
+        while cursor <= digit/2:
+            
+            if digit % cursor == 0:
+                is_prime = 0
+                break
+
+            cursor += 1
+
+            
+        if is_prime:
+            print(digit)
+
+        digit += 1
